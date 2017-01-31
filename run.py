1a699ca9e00f409b8d5e6d44265b314932a06d11
Jorn Tuyls <tuylsj@opsi-master.informatik.uni-freiburg.de>
2017-01-31 18:41:37 +0100

    figures

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
 from variational_autoencoder import VariationalAutoEncoder
 vae = VariationalAutoEncoder()
-vae.main(data_set="celeb_data", num_epochs=301, batch_size=100, downsampling=None)
+vae.main(data_set="celeb_data", num_epochs=300, batch_size=100, downsampling=None)
 
 
